20e9ac85836059bab95322f516cc882f234ca170
osparking <jbpark03@gmail.com>
2023-09-25 22:25:48 +0900

    initial: request-scoped bean

---
+++ src/main/java/ssh_09_ex1/SqCh09Ex1Application.java
@@ -0,0 +1,13 @@
+package ssh_09_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh09Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh09Ex1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ssh_09_ex1/SqCh09Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ssh_09_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh09Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

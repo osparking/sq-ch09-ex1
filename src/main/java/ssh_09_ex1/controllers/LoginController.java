0bcd8e2692a2b5a84484a7feb0236a51e763b77a
osparking <jbpark03@gmail.com>
2023-09-28 22:09:17 +0900

    request-scoped 빈을 로그인 처리기가 사용했다.

--- src/main/java/ssh_09_ex1/controllers/LoginController.java
+++ src/main/java/ssh_09_ex1/controllers/LoginController.java
@@ -6,24 +6,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.AllArgsConstructor;
+
 @Controller
+@AllArgsConstructor
 public class LoginController {
 	@GetMapping("/")
 	public String loginForm() {
 		return "login.html";
 	}
-	
+
+	private final LoginProcessor loginProcessor;
+
 	@PostMapping("/")
-	public String processLogin(@RequestParam String username, 
+	public String processLogin(@RequestParam String username,
 			@RequestParam String password, Model page) {
-		String message = null;
-		if ("natalie".equals(username) && "1234".equals(password)) {
-			message = "로그인 되었습니다";
+		loginProcessor.setUsername(username);
+		loginProcessor.setPassword(password);
+		if (loginProcessor.validLogin()) {
+			page.addAttribute("message", "로그인 되었습니다");
 		} else {
-			message = "아이디 혹은 비밀번호 오류입니다";
+			page.addAttribute("message", "아이디 혹은 비밀번호 오류입니다");
 		}
-		page.addAttribute("message", message);
 		return "login.html";
 	}
-
 }
